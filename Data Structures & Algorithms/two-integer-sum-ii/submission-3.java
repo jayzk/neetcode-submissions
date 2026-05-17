51365328571880df6df57409535cc03b8ceb75d8
Jayden Yeo <jaydenzkyeo@gmail.com>
2026-05-17 01:21:50 -0500

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        for(int i = 0; i < numbers.length; i++) {
+            int findNum = target - numbers[i];
+            int result = binarySearch(numbers, findNum); //find pos of second num
+
+            if(result != -1) {
+                return new int[]{i+1, result+1};
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+
+    public int binarySearch(int[] numbers, int findNum) {
+        int start = 0;
+        int end = numbers.length - 1;
+        int mid = (start + end) / 2;
+
+        while(start <= end) {
+            if(numbers[mid] == findNum) {
+                return mid;
+            } else if(numbers[mid] > findNum) { //search left
+                end = mid-1;
+            } else if(numbers[mid] < findNum) { //search right
+                start = mid+1;
+            }
+
+            mid = (start + end) / 2;
+        }
+
+        return -1; //no result
+    }
+}
